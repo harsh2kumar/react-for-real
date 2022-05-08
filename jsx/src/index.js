b1db598116ebcbf150cb112d53d6b4a32e294dd3
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2022-05-08 17:19:25 -0400

    Section 2: Completed

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,14 +1,27 @@
 // import the React and ReactDom libraries
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
+function getButtonText() {
+  const buttonText = "Click on Me!";
+  return buttonText;
+}
 // Create a React component
 const App = () => {
-    return <div>Hi there!</div>;
+  const buttonText = { text: "Click on Me!" };
+  const labelText = "Enter Name: ";
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 // Take the React component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector("#root")
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
