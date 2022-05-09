3bd6267f1a506350d54c18acbb883223d6464d19
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2022-05-09 15:38:13 -0400

    Section 3 Complete

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "@faker-js/faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        avatar={faker.image.image()}
-        author="Sam"
-        timeAgo="Today at 2:00PM"
-        commentText="Awesome!"
-      />
-      <CommentDetail
-        avatar={faker.image.image()}
-        author="Harsh"
-        timeAgo="Today at 4:00PM"
-        commentText="Great Work!"
-      />
-      <CommentDetail
-        avatar={faker.image.image()}
-        author="Leia"
-        timeAgo="Yesterday at 6:00PM"
-        commentText="Nice blog post!"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.image()}
+          author="Sam"
+          timeAgo="Today at 2:00PM"
+          commentText="Awesome!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.image()}
+          author="Harsh"
+          timeAgo="Today at 4:00PM"
+          commentText="Great Work!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.image()}
+          author="Leia"
+          timeAgo="Yesterday at 6:00PM"
+          commentText="Nice blog post!"
+        />
+      </ApprovalCard>
     </div>
   );
 };
