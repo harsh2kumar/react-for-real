beb25072ce05b5548353a40ece7404fc48633962
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2022-05-10 10:50:05 -0400

    Section 6: introduction to lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -24,6 +24,14 @@ class App extends React.Component {
     );
   }
 
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
+  }
+
+  componentDidUpdate() {
+    console.log("My component was re-rendered");
+  }
+
   //React says we have to define render
   render() {
     if (this.state.errorMessage && !this.state.lat) {
