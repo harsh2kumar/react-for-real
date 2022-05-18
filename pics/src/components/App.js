21acfdf477c0ed2bd020b942be540ffc86b28cd4
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2022-05-18 10:44:01 -0400

    Section 8.97: Added code for async call using promise-then syntax

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit = (term) => {
-    console.log(term);
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID jTPv-_x4gI-AmHik7GisI0qwip6a3P6LPL3NadIHLFU",
+        },
+      })
+      .then((response) => {
+        console.log(response.data.results);
+      });
   };
   render() {
     return (
